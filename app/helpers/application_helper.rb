b62e4094f16672be25a2f708115a8adb7e6eb269
Wang Le <wangle@e-accent.com>
2011-05-25 16:40:06 +0800

    playing with nokogiri to make tags for ordered/unordered list useable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,29 @@
 
 module ApplicationHelper
   def wrap(content)
-    content.
-      gsub(/<(\/?)s>/, "<\\1strikethrough>").
-      gsub(/<p>/, "\n").
-      gsub(/<\/p>/, "").
-      gsub(/<\/?[ou]l>/, "").
-      gsub /<li>(.*)<\/li>/ do
-        "• #{$1}"
-      end
+    markup = Nokogiri::HTML(content)
+    markup.xpath('//s').each do |item|
+      item.name = "strikethrough"
+    end
+    markup.xpath('//ul/li').each do |item|
+      item.replace "* #{item.inner_html}"
+    end
+    markup.xpath('//ol/li').each_with_index do |item, index|
+      item.replace "#{index + 1}. #{item.inner_html}"
+    end
+    markup.xpath('//ol').each do |list|
+      list.replace list.inner_html
+    end
+    markup.xpath('//ul').each do |list|
+      list.replace list.inner_html
+    end
+    markup.xpath('//p').each do |paragraph|
+      paragraph.replace "\n" + paragraph.inner_html
+    end
+
+    markup.to_s.
+      gsub(/.*<body>/m, "").
+      gsub(/<\/body.*>/m, "").
+      strip
   end
 end
