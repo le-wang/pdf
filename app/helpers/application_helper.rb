06f45a3db370200eac40c8033d39dfc433566447
Wang Le <wangle@e-accent.com>
2011-05-25 15:30:19 +0800

    adding primitive support for p and li tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
+# encoding: utf-8
 # highlight: used in app/views/documents/show.pdf.prawn
 
 module ApplicationHelper
   def wrap(content)
-    content = content.gsub /<(\/?)s>/, "<\\1strikethrough>"
+    content.
+      gsub(/<(\/?)s>/, "<\\1strikethrough>").
+      gsub(/<p>/, "\n").
+      gsub(/<\/p>/, "").
+      gsub(/<\/?[ou]l>/, "").
+      gsub /<li>(.*)<\/li>/ do
+        "• #{$1}"
+      end
   end
 end
--- app/views/documents/show.pdf.prawn
+++ app/views/documents/show.pdf.prawn
@@ -2,4 +2,4 @@
 # pdf.text only recognizes <strikethrough> tag, not <s>, so wrap it.
 # see http://prawn.majesticseacreature.com/docs/0.11.1/Prawn/Text.html
 
-pdf.text(wrap(sanitize(@document.content, :tags => %w(p i b s u))), :inline_format => true)
+pdf.text(wrap(sanitize(@document.content, :tags => %w(p i b s u ul ol li))), :inline_format => true)
